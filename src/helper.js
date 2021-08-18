f6b81ee7b4455197a6cefd95fbcc69a77ab2e5b6
clause <claussdev5@gmail.com>
2021-08-18 11:30:19 -0400

    all done

---
+++ src/helper.js
@@ -0,0 +1,39 @@
+//Obtiene la diferencia de anios
+export function obtenerDiferenciaYear(year){
+    return new Date().getFullYear() - year;
+}
+
+
+//Calcula el total a pagar segun la marca
+
+export function  calcularMarca(marca){
+    let incremento;
+
+    switch (marca) {
+        case 'europeo':
+            incremento = 1.3;
+            break;
+        case 'americano':
+            incremento = 1.15;
+            break;
+        case 'asiatico': 
+            incremento = 1.05;
+            break;
+    
+        default:
+            break;
+    }
+    return incremento;
+}
+
+//Clcula el tipo de plan
+
+export function obtenerPlan( plan ){
+    return (plan === 'basico') ? 1.2 : 1.5;
+}
+
+//Capitalize
+
+export function primeraMayuscula (texto ){
+    return texto.charAt(0).toUpperCase() + texto.slice(1);
+}
